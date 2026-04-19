f72ec1befd9108e8349617a85a7935912e4f6251
secretmomo <secret_momo@protonmail.com>
2026-04-19 09:19:08 +0800

    chore: add n or null function

--- packages/notion/src/index.ts
+++ packages/notion/src/index.ts
@@ -2,9 +2,14 @@ import {
   Client,
   type NumberPropertyItemObjectResponse,
   type PageObjectResponse,
+  type RichTextItemResponse,
 } from '@notionhq/client';
 
-export type { NumberPropertyItemObjectResponse, PageObjectResponse };
+export type { NumberPropertyItemObjectResponse, PageObjectResponse, RichTextItemResponse };
+
+export function n(num?: number) {
+  return num ?? null;
+}
 
 export const notion = new Client({
   auth: process.env.NOTION_AUTH_TOKEN,

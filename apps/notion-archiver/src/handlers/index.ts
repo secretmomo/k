224e953d305a49b9e3bc66caa1494ab9ee93ebe4
secretmomo <secret_momo@protonmail.com>
2026-04-22 22:19:04 +0800

    feat: 增加容错处理，避免出错导致程序退出

--- apps/notion-archiver/src/handlers/index.ts
+++ apps/notion-archiver/src/handlers/index.ts
@@ -1,13 +1,28 @@
+import { sendErrorCardMessage } from '@k/notifier/lark';
+import consola from 'consola';
+
 import type { Handler } from './handler';
 import { UnknownHandler } from './unknown-handler';
 import { WebpageHandler } from './webpage-handler';
 
 const handlers: Handler[] = [new WebpageHandler(), new UnknownHandler()];
 
-export function handle(message_id: string, text: string) {
+export async function handle(message_id: string, text: string) {
   for (const handler of handlers) {
     if (handler.isMatch(text)) {
-      return handler.process(message_id, text);
+      try {
+        await handler.process(message_id, text);
+      } catch (e: unknown) {
+        consola.error(e);
+
+        if (e instanceof Error) {
+          await sendErrorCardMessage(`存在未捕获的异常：\n${e.message}。`);
+        } else {
+          await sendErrorCardMessage('存在未捕获的异常：未知错误。');
+        }
+      }
+
+      return;
     }
   }
 }
